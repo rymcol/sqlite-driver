6aa54ec4059af8a5f14203f9239c10aaf6a0968e
Ryan Collins <ryan@ryanmcollins.com>
2016-08-05 09:32:12 -0400

    Bump Fluent Version to 9
    Updates Fluent version to 9 and renames qutheory username to vapor

--- Package.swift
+++ Package.swift
@@ -3,7 +3,7 @@ import PackageDescription
 let package = Package(
     name: "FluentSQLite",
     dependencies: [ 
-        .Package(url: "https://github.com/qutheory/sqlite.git", majorVersion: 0, minor: 1),
-        .Package(url: "https://github.com/qutheory/fluent.git", majorVersion: 0, minor: 7)
+        .Package(url: "https://github.com/vapor/sqlite.git", majorVersion: 0, minor: 1),
+        .Package(url: "https://github.com/vapor/fluent.git", majorVersion: 0, minor: 9)
     ]
 )
